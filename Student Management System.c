c271a1d37f8c1eaeaca76ea77cc78617a88e1c95
Adhvika-aj <adhvika.aj@gmail.com>
2024-08-05 18:24:00 +0530

    Update and rename 1.c to Student Management System.c

--- Student Management System.c
+++ Student Management System.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-
 #define MAX_STUDENTS 100
-
 typedef struct {
     int id;
     char name[50];
     float grade;
 } Student;
-
 int main() {
     Student students[MAX_STUDENTS];
     int count = 0;
     int choice;
-
     do {
         printf("\n1. Add Student\n");
         printf("2. View Students\n");
         printf("3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-
         if (choice == 1) {
             if (count < MAX_STUDENTS) {
                 printf("Enter student ID: ");
@@ -38,6 +33,5 @@ int main() {
             }
         }
     } while (choice != 3);
-
     return 0;
 }
